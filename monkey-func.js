b5f2338605c40cabed9fcfce89bc9a43bcf35920
Gamersans55 <Ezrawilliamnelson@gmail.com>
2020-07-23 00:20:55 -0700

    Poppy corn

---
+++ monkey-func.js
@@ -0,0 +1,15 @@
+function monkeyFunc(monkeyBool){
+	if(monkeyBool==0){
+	 return "no taco"
+	}
+        if(monkeyBool==1){
+         return taco}
+}
+
+var taco;
+
+var firesauce;
+
+firesauce="packet of deliciousness";
+
+taco=firesauce;
